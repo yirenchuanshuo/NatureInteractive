978898c121746e43a4cb098762efc87f28af45fc
Rebornqin <rebornqin@tencent.com>
2025-05-06 15:37:14 +0800

    CustomRenderPass

---
+++ Source/NatureInteractive/Private/Common/InteractiveInfoRenderingPass.cpp
@@ -0,0 +1,23 @@
+﻿#include "Common/InteractiveInfoRenderingPass.h"
+
+#include "RenderGraphUtils.h"
+
+FInteractiveInfoCustomRenderPassBase::FInteractiveInfoCustomRenderPassBase(const FString& InDebugName,ERenderMode InRenderMode, ERenderOutput InRenderOutput,
+	const FIntPoint& InRenderTargetSize):FCustomRenderPassBase(InDebugName, InRenderMode, InRenderOutput, InRenderTargetSize)
+{
+}
+
+FInteractiveRenderingDepthPass::FInteractiveRenderingDepthPass(const FIntPoint& InRenderTargetSize)
+	:FInteractiveInfoCustomRenderPassBase(TEXT("InteractiveInfoDepthPass"), ERenderMode::DepthPass, ERenderOutput::DeviceDepth, InRenderTargetSize)
+{
+}
+
+void FInteractiveRenderingDepthPass::OnPreRender(FRDGBuilder& GraphBuilder)
+{
+	RenderTargetTexture = RegisterExternalTexture(GraphBuilder,InteractiveInfoRenderTarget->GetRenderTargetTexture(), TEXT("InteractiveInfoDepthPass"));
+}
+
+void FInteractiveRenderingDepthPass::SetRenderTargetTexture(FRenderTarget* InRenderTargetTexture)
+{
+	InteractiveInfoRenderTarget = InRenderTargetTexture;
+}
---
+++ Source/NatureInteractive/Public/Common/InteractiveInfoRenderingPass.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "Rendering/CustomRenderPass.h"
+
+class FInteractiveInfoCustomRenderPassBase : public FCustomRenderPassBase
+{
+public:
+	FInteractiveInfoCustomRenderPassBase(const FString& InDebugName, ERenderMode InRenderMode, ERenderOutput InRenderOutput, const FIntPoint& InRenderTargetSize);
+
+	// Abstract class :
+	virtual const FName& GetTypeName() const PURE_VIRTUAL(FInteractiveInfoCustomRenderPassBase::GetTypeName, static FName Name; return Name;);
+};
+
+
+// ----------------------------------------------------------------------------------
+
+class FInteractiveRenderingDepthPass final : public FInteractiveInfoCustomRenderPassBase
+{
+public:
+	IMPLEMENT_CUSTOM_RENDER_PASS(FInteractiveRenderingDepthPass);
+
+	FInteractiveRenderingDepthPass(const FIntPoint& InRenderTargetSize);
+
+	virtual void OnPreRender(FRDGBuilder& GraphBuilder) override;
+
+	void SetRenderTargetTexture(FRenderTarget* InRenderTargetTexture);
+
+	FRenderTarget* InteractiveInfoRenderTarget = nullptr;
+};
