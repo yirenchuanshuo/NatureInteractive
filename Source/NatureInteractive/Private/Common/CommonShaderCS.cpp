a91e5ec298de904dc9e7810c5f84e1b5800793f6
yirenchuanshuo <935678887@qq.com>
2026-05-29 17:20:24 +0800

    微调

--- Source/NatureInteractive/Private/Common/CommonShaderCS.cpp
+++ Source/NatureInteractive/Private/Common/CommonShaderCS.cpp
@@ -48,3 +48,33 @@ void CommonShaderCS::AddHeightToNormalPass(FRDGBuilder& GraphBuilder, FRDGTextur
 			FComputeShaderUtils::Dispatch(RHICmdList, ComputeShader, *Parameters, GroupCount);
 		});
 }
+
+class FGaussianBlurCS :public FGlobalShader
+{
+	DECLARE_GLOBAL_SHADER(FGaussianBlurCS);
+	SHADER_USE_PARAMETER_STRUCT(FGaussianBlurCS, FGlobalShader);
+	class FBlurDirection : SHADER_PERMUTATION_INT("BLUR_DIRECTION", 2); // 0:H, 1:V
+	using FPermutationDomain = TShaderPermutationDomain<FBlurDirection>;
+
+	BEGIN_SHADER_PARAMETER_STRUCT(FParameters, )
+		SHADER_PARAMETER_RDG_TEXTURE(Texture2D<float4>,       BlurInput)
+		SHADER_PARAMETER_RDG_TEXTURE_UAV(RWTexture2D<float4>, BlurOutput)
+		SHADER_PARAMETER(int32,       BlurRadius)
+		SHADER_PARAMETER(float,       BlurSigma)
+		SHADER_PARAMETER(FIntPoint,   TexSize)
+	END_SHADER_PARAMETER_STRUCT()
+
+public:
+	static bool ShouldCompilePermutation(const FGlobalShaderPermutationParameters& Parameters)
+	{
+		return IsFeatureLevelSupported(Parameters.Platform, ERHIFeatureLevel::SM5);
+	}
+	static void ModifyCompilationEnvironment(const FGlobalShaderPermutationParameters& Parameters, FShaderCompilerEnvironment& OutEnvironment)
+	{
+		FGlobalShader::ModifyCompilationEnvironment(Parameters, OutEnvironment);
+		OutEnvironment.SetDefine(TEXT("SIZE_X"), CommonShaderCS::ThreadX);
+		OutEnvironment.SetDefine(TEXT("SIZE_Y"), CommonShaderCS::ThreadY);
+		OutEnvironment.SetDefine(TEXT("SIZE_Z"), CommonShaderCS::ThreadZ);
+	}
+};
+IMPLEMENT_SHADER_TYPE(, FGaussianBlurCS, TEXT("/Plugin/NatureInteractive/Common/GaussianBlur.usf"), TEXT("GaussianBlurCS"), SF_Compute);
--- Source/NatureInteractive/Private/Fluid/FluidHeightToNormalPass.cpp
+++ Source/NatureInteractive/Private/Fluid/FluidHeightToNormalPass.cpp
@@ -3,33 +3,8 @@
 #include "DataDrivenShaderPlatformInfo.h"
 #include "RenderGraphBuilder.h"
 #include "RenderGraphUtils.h"
-#include "ShaderParameterStruct.h"
+#include "Common/CommonShaderCS.h"
 #include "Fluid/FluidInteractiveRenderData.h"
-
-
-class FFluidHeightToNormalCS : public FGlobalShader
-{
-	DECLARE_SHADER_TYPE(FFluidHeightToNormalCS, Global);
-	SHADER_USE_PARAMETER_STRUCT(FFluidHeightToNormalCS, FGlobalShader);
-	BEGIN_SHADER_PARAMETER_STRUCT(FParameters, )
-	SHADER_PARAMETER_RDG_TEXTURE(Texture2D,FluidInput)
-	SHADER_PARAMETER_RDG_TEXTURE_UAV(RWTexture2D<float4>, FluidOutput)
-	END_SHADER_PARAMETER_STRUCT()
-
-	public:
-	static bool ShouldCompilePermutation(const FGlobalShaderPermutationParameters& Parameters)
-	{
-		return IsFeatureLevelSupported(Parameters.Platform, ERHIFeatureLevel::SM5);
-	}
-	static void ModifyCompilationEnvironment(const FGlobalShaderPermutationParameters& Parameters, FShaderCompilerEnvironment& OutEnvironment)
-	{
-		FGlobalShader::ModifyCompilationEnvironment(Parameters, OutEnvironment);
-		OutEnvironment.SetDefine(TEXT("SIZE_X"), FFluidInteractiveRenderData::ThreadX);
-		OutEnvironment.SetDefine(TEXT("SIZE_Y"), FFluidInteractiveRenderData::ThreadY);
-		OutEnvironment.SetDefine(TEXT("SIZE_Z"), FFluidInteractiveRenderData::ThreadZ);
-	}
-};
-IMPLEMENT_SHADER_TYPE(,FFluidHeightToNormalCS, TEXT("/Plugin/NatureInteractive/FluidInteractive/FluidHeightToNormal.usf"), TEXT("HeightToNormalCS"), SF_Compute);
 
 // ReSharper disable once CppMemberFunctionMayBeStatic
 void FFluidHeightToNormalPass::Draw(FRHICommandListImmediate& RHICommandList,
@@ -44,26 +19,9 @@ void FFluidHeightToNormalPass::Draw(FRHICommandListImmediate& RHICommandList,
 	
 	
 	FRDGTextureRef FluidOutput = GraphBuilder.CreateTexture(Desc, TEXT("FluidOutput"));
-	FRDGTextureUAVRef FluidOutputUAV = GraphBuilder.CreateUAV(FluidOutput);
-	
 	FRDGTextureRef FluidInput = RegisterExternalTexture(GraphBuilder, RenderData->FluidFinalData->GetRenderTargetTexture(), TEXT("FluidInput"));
 	
-
-	TShaderMapRef<FFluidHeightToNormalCS> FluidHeightToNormalComputeShader(GetGlobalShaderMap(RenderData->FeatureLevel));
-	FFluidHeightToNormalCS::FParameters* FluidHeightToNormalParameters = GraphBuilder.AllocParameters<FFluidHeightToNormalCS::FParameters>();
-	
-	FluidHeightToNormalParameters->FluidInput = FluidInput;
-	FluidHeightToNormalParameters->FluidOutput = FluidOutputUAV;
-	
-	auto GroupCount = FIntVector(RenderData->SizeX / FFluidInteractiveRenderData::ThreadX, RenderData->SizeY / FFluidInteractiveRenderData::ThreadY, 1);
-	GraphBuilder.AddPass(
-	RDG_EVENT_NAME("FluidInteractiveDiffusionComputeShader"),
-	FluidHeightToNormalParameters,
-	ERDGPassFlags::Compute,
-	[FluidHeightToNormalComputeShader,&FluidHeightToNormalParameters,GroupCount](FRHIComputeCommandList& RHICmdList)
-	{
-		FComputeShaderUtils::Dispatch(RHICmdList, FluidHeightToNormalComputeShader, *FluidHeightToNormalParameters,GroupCount);
-	});
+	CommonShaderCS::AddHeightToNormalPass(GraphBuilder,FluidInput,FluidOutput,RenderData->FeatureLevel,TEXT("FluidInteractiveHeightToNormal"));
 	
 	FRDGTextureRef FluidNormal = RegisterExternalTexture(GraphBuilder, RenderData->FluidNormalData->GetRenderTargetTexture(), TEXT("FluidNormal"));
 	AddCopyTexturePass(GraphBuilder, FluidOutput, FluidNormal, FRHICopyTextureInfo());
